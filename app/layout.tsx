60f51854b5fc8ab8247193cd05ce4a1eef60f25a
Aman Sahu <aman.sahu.as96@gmail.com>
2024-08-10 00:53:01 +0530

    added assets,font,metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,43 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk as SpaceGrotest } from "next/font/google";
 import { ReactNode } from "react";
 import "./globals.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = SpaceGrotest({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevQuery",
+  description:
+    "DevQuery is a leading Q&A platform where developers solve coding challenges, share knowledge, and collaborate on software development.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
