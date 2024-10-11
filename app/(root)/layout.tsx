3cd661b7c0b4860247b883ff0b7b3e1a529649c1
Aman Sahu <aman.sahu.as96@gmail.com>
2024-10-11 19:23:38 +0530

    fixes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,11 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
-import Navbar from "@/components/shared/navbar/Navbar";
+import MobileNavbar from "@/components/shared/navbar/MobileNavbar";
+import Theme from "@/components/shared/navbar/Theme";
 import RightSidebar from "@/components/shared/RightSidebar";
+import GlobalSearch from "@/components/shared/search/GlobalSearch";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Link } from "lucide-react";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
@@ -15,6 +20,44 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <RightSidebar />
       </div>
     </main>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed top-0 z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          alt="DevQuery"
+          src="/assets/images/site-logo.svg"
+          width={23}
+          height={23}
+        />
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Query</span>
+        </p>
+      </Link>
+      <GlobalSearch />
+      <div className="flex-between gap-5">
+        <Theme />
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+        <MobileNavbar />
+      </div>
+    </nav>
   );
 };
 
